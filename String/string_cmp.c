44481603c6756e374959dcd0f4d5c96c71ca89ae
mahin273 <md.mahin.bd18@gmail.com>
2023-04-06 09:46:20 +0600

    update

--- String/string_cmp.c
+++ String/string_cmp.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main()
 {
-    int result = strcmp("hello","all");//use strcmpi to ignore case
+    int result = strcmp("hello","Hello");//use strcmpi to ignore case
     printf("%d",result);
 
     return 0;
