336b754f3e6644df24407606a53dada14b56bc63
mahin273 <md.mahin.bd18@gmail.com>
2023-04-07 19:54:00 +0600

    print prime number from 2 to n

---
+++ 2nd Trimester/Function/print_prime_num_from_0_to_n.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int prime(int);
+int main()
+{
+    int i,n;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+    for ( i = 2; i <= n; i++)
+    {
+        if (prime(i) == 1)
+        {
+            printf("%d\n",i);
+        }
+        
+    }
+    
+    
+
+return 0;
+}
+int prime(int num)
+{
+    int i;
+    int flag = 1;
+    for ( i = 2; i <num; i++)
+    {
+        if (num%i == 0)
+        {
+            flag =0;
+            break;
+        }
+        
+    }
+    return flag;
+
+    
+
+}
