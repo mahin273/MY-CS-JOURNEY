965f4a1e69979c0bfe004d2496b65be8d02c5c2f
mahin273 <md.mahin.bd18@gmail.com>
2023-05-04 23:16:29 +0600

    update

---
+++ 2nd Trimester/Recursion/count_digit.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int count(int);
+int main()
+{
+    int n,Digits;
+    scanf("%d",&n);
+    Digits=count(n);
+    printf("Result = %d",Digits);
+     
+
+
+}
+
+int count(int x)
+{
+if (x==0)
+{
+    return 0;
+}
+else
+{
+    return 1+count(x/10);
+}
+
+}
